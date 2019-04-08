338e00ae55e9463de69d81fa203e2449b8142c08
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2019-04-08 17:07:59 +0600

    first commit

---
+++ sysadminTeamsChromeExtension/loadcss.js
@@ -0,0 +1,20 @@
+// Inject the css file into the head element
+function appendStyleNode(id, href) {
+    var cssNode = document.createElement('link');
+    cssNode.type = 'text/css';
+    cssNode.rel = 'stylesheet';
+    cssNode.id = id;
+    cssNode.href = href;
+    document.getElementsByTagName('head')[0].appendChild(cssNode);
+}
+
+// window onload override
+var oldWindowOnload = window.onload;
+window.onload = function() {
+
+    appendStyleNode('sa_custom', 'https://github.com/github/hubot/blob/master/sa.css');
+
+    if (oldWindowOnload && typeof(oldWindowOnload) === 'function') {
+        oldWindowOnload();
+    }
+};
